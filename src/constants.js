7651ed814f6b6ffb8d8f61a080f7f2a4f3bc3304
ZhouYingKai1 <13625695226@163.com>
2017-10-18 16:45:53 +0800

    提交修改

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   path: 'blog',
   // host: 'http://192.168.7.58:80/'
-  host: 'http://' + window.location.host + '/'
+  host: window.location.hostname == 'localhost'?'http://ywjgame.com/' : 'http://' + window.location.host + '/'
 }
 

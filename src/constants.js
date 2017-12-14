355015b4a3f0e6b3e6157ed84d1fd7d98ec4a6c2
zhouyingkai <zyk@zykMac-Pro.local>
2017-12-14 15:22:09 +0800

    提交

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
 module.exports = {
   path: 'blog',
-  // host: 'http://192.168.7.58:80/'
   host: window.location.hostname == 'localhost'?'http://ywjgame.com/' : 'http://' + window.location.host + '/'
 }
 
